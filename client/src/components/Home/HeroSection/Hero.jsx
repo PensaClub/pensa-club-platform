8c67012e61e246d0dde553a43fa09e394ee5d292
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-05-14 18:38:56 +0300

    Create home component
    Create Hero section
    Add hero slider
    Hero slider to be made mobile first

---
+++ client/src/components/Home/HeroSection/Hero.jsx
@@ -0,0 +1,25 @@
+import Testimonials from './Testimonials/Testimonials';
+import './hero.css';
+
+
+export const Hero = () => {
+    return (
+        <>
+            <section className="hero-section">
+                <div className="parent-hero">
+                    <div className="left-side">
+                        <h1>Explore Our Resources</h1>
+                        <p>Discover a vibrant community of pensioners, connect with like-minded individuals, and explore a world of opportunities.</p>
+                        <div className="btn-hero">
+                            <a href="">View Resources</a>
+                            <a href="">Join Community</a>
+                        </div>
+                    </div>
+
+                    <Testimonials />
+                </div>
+            </section>
+
+        </>
+    )
+}
---
+++ client/src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import { Hero } from './HeroSection/Hero'
+import './home.css'
+
+export const Home = () => {
+    return (
+        <>
+            <div className="home-container">
+                <Hero />
+
+            </div>
+        </>
+
+    )
+}
