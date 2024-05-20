6ab959146735df1a654201cbab6488b5927453ce
Dafy Yasenova <dafy_yasenova@abv.bg>
2024-05-20 08:39:35 +0300

    update login and register validations
    add signUp utils

--- client/src/components/LoginRegister/Login.jsx
+++ client/src/components/LoginRegister/Login.jsx
@@ -1,66 +1,40 @@
-import './loginRegister.css';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
 
-export const Login = ({ navToRegister }) => {
+import './loginRegister.css';
+import { trimFields, resetFields, validatePhoneNumber, validatePassword } from '../../utils/signUp';
 
+export const Login = ({ navToRegister }) => {
   const [phoneNumber, setPhoneNumber] = useState('');
   const [password, setPassword] = useState('');
   const [errors, setErrors] = useState({});
 
-  const phoneRegex = /^(\+?\d{1,3})?\s?\d{9}$/;
-
-  const trimFields = () => {
-    setPhoneNumber(phoneNumber.trim());
-    setPassword(password.trim());
-
+  const handleTrimFields = () => {
+    const [trimmedPhoneNumber, trimmedPassword] = trimFields([phoneNumber, password]);
+    setPhoneNumber(trimmedPhoneNumber);
+    setPassword(trimmedPassword);
   };
 
-  const validatePhoneNumber = () => {
-    if (!phoneNumber) {
-      setErrors(prevErrors => ({
-        ...prevErrors, phoneNumber: "Полето е задължително!"
-      }));
-    } else if (!phoneRegex.test(phoneNumber)) {
-      setErrors(prevErrors => ({
-        ...prevErrors, phoneNumber: "Невалиден телефонен номер!"
-      }));
-    } else {
-      setErrors(prevErrors => ({
-        ...prevErrors, phoneNumber: ''
-      }));
-    }
-  };
-
-  const validatePassword = () => {
-    if (!password) {
-      setErrors(prevErrors => ({
-        ...prevErrors, password: "Полето е задължително!"
-      }));
-    } else if (password.length < 3) {
-      setErrors(prevErrors => ({
-        ...prevErrors, password: "Паролата трябва да бъде поне 3 символа!"
-      }));
-    } else {
-      setErrors(prevErrors => ({
-        ...prevErrors, password: ''
-      }));
-    }
+  const handleResetFields = () => {
+    resetFields([setPhoneNumber, setPassword, () => setErrors({})]);
   };
 
   const validate = () => {
-    validatePhoneNumber();
-    validatePassword();
-    return Object.keys(errors).some(key => !errors[key]);
+    validatePhoneNumber(phoneNumber, setErrors);
+    validatePassword(password, setErrors);
+    return Object.values(errors).every(error => !error);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    trimFields();
+    handleTrimFields();
+
     if (validate()) {
-      console.log('phone number:', phoneNumber)
-      console.log('password:', password)
+      console.log('phone number:', phoneNumber);
+      console.log('password:', password);
       console.log('Valid form');
+      handleResetFields(); 
+
     } else {
       console.log('Invalid form');
     }
@@ -78,26 +52,25 @@ export const Login = ({ navToRegister }) => {
           className="input"
           value={phoneNumber}
           onChange={(e) => setPhoneNumber(e.target.value)}
-          onBlur={() => { trimFields(); validatePhoneNumber(); }}
+          onBlur={() => { handleTrimFields(); validatePhoneNumber(phoneNumber, setErrors); }}
         />
         {errors.phoneNumber && <p className="error">{errors.phoneNumber}</p>}
 
         <label className="label" htmlFor="password">Парола</label>
-        <input type="password"
+        <input
+          type="password"
           placeholder="Парола"
           className="input"
-          alue={password}
+          value={password}
           onChange={(e) => setPassword(e.target.value)}
-          onBlur={() => { trimFields(); validatePassword(); }}
+          onBlur={() => { handleTrimFields(); validatePassword(password, setErrors); }}
         />
-
         {errors.password && <p className="error">{errors.password}</p>}
 
-        <a href="#" className="link">Забравена парола?</a>
+        <Link to="#" className="link">Забравена парола?</Link>
         <button className="btn">Вход</button>
         <Link to="/sign-up" className="link link-hidden" onClick={navToRegister}>Нямате акаунт? <span>Регистрация</span></Link>
       </form>
     </div>
-  )
-}
-
+  );
+};
