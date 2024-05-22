c16607ff62ba1db728ccfb6f6f497eca7c6008af
nymuelis <daniel.tsv.georgiev@gmail.com>
2024-05-22 16:52:37 +0300

    Add the rest regions

--- server/regions-data/index.js
+++ server/regions-data/index.js
@@ -1,19 +1,5 @@
 const changeInfoFormat = require('./changeInfoFormat');
 
-const input = [
-    {
-        "PostCode": null,
-        "Bbox": {
-            "MinX": 288704.622,
-            "MinY": 4708943.115,
-            "MaxX": 299006.919,
-            "MaxY": 4719117.644
-        },
-        "AdmId": 3924,
-        "Text": "Радомир",
-        "Code": "61577",
-        "FullText": "Радомир (61577)"
-    }
-]
+const input = []
 
 changeInfoFormat(input);
