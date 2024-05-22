86444953157c574eaff413408010d4ccc54fc47e
nymuelis <daniel.tsv.georgiev@gmail.com>
2024-05-22 15:50:23 +0300

    Add 10 more regions, half done

--- server/regions-data/index.js
+++ server/regions-data/index.js
@@ -1,5 +1,19 @@
 const changeInfoFormat = require('./changeInfoFormat');
 
-const input = []
+const input = [
+    {
+        "PostCode": null,
+        "Bbox": {
+            "MinX": 288704.622,
+            "MinY": 4708943.115,
+            "MaxX": 299006.919,
+            "MaxY": 4719117.644
+        },
+        "AdmId": 3924,
+        "Text": "Радомир",
+        "Code": "61577",
+        "FullText": "Радомир (61577)"
+    }
+]
 
 changeInfoFormat(input);
