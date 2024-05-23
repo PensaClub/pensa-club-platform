69171aecc1bf94da830ecd8487488f43529e99b7
Kristiyan <kristiyan.konov@forkpoint.com>
2024-05-23 23:36:22 +0300

    Debug commit

--- server/src/sequelize/models/index.js
+++ server/src/sequelize/models/index.js
@@ -9,6 +9,7 @@ const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.js")[env];
 const db = {};
 
+console.log(config)
 const sequelize = new Sequelize(
   config.database_uri,{
     dialectOptions: {
