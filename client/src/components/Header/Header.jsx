7e3f50f8727906931b3c5257d1666024031ad17d
SimonaGancheva <ilievas95@gmail.com>
2024-05-30 13:52:11 +0300

    Style language switcher button

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,33 +1,44 @@
-import './header.css'
+import './header.css';
 
-import { useContext, useEffect, useRef, useState } from "react";
-import { Link } from 'react-router-dom'
-import { UserContext } from "../contexts/UserContext";
+import { useContext, useEffect, useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { UserContext } from '../contexts/UserContext';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowRightToBracket, faAddressCard, faUser, faArrowRightFromBracket, faMap, faBars, faCircleQuestion, faCircleExclamation } from '@fortawesome/free-solid-svg-icons';
-import { LanguageSwitcher } from "./LanguageSwitcher";
-import { useTranslation } from "react-i18next";
+import {
+  faArrowRightToBracket,
+  faAddressCard,
+  faUser,
+  faArrowRightFromBracket,
+  faMap, 
+  faBars, faCircleQuestion, faCircleExclamation,
+  faGlobe,
+} from '@fortawesome/free-solid-svg-icons';
+
+import { useTranslation } from 'react-i18next';
 import AlertModal from "./AlertModal/AlertModal";
 
 export const Header = () => {
-    const [isMenuOpen, setMenuOpen] = useState(false);
-    const [isDropdownOpen, setDropdownOpen] = useState(false);
+  const { t, i18n } = useTranslation();
+
+  const [isMenuOpen, setMenuOpen] = useState(false);
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
     const [isModalOpen, setModalOpen] = useState(false);
     const { isAuthentication, isFinish } = useContext(UserContext)
     const [finishProfile, setFinishProfile] = useState(false);
+  const [currLang, setCurrLang] = useState('en');
 
     const dropdownRef = useRef(null)
 
-    const handleClickOutside = (event) => {
-        if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-            setDropdownOpen(false)
-            setMenuOpen(!isMenuOpen)
-        }
+  const handleClickOutside = (event) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setDropdownOpen(false);
+      setMenuOpen(!isMenuOpen);
     }
-    const handleDropdownToggle = () => {
-        setDropdownOpen(!isDropdownOpen);
-        setMenuOpen(!isMenuOpen)
-    };
+  };
+  const handleDropdownToggle = () => {
+    setDropdownOpen(!isDropdownOpen);
+    setMenuOpen(!isMenuOpen);
+  };
 
     const handleDropdownItemClick = () => {
         setDropdownOpen(false);
@@ -50,7 +61,12 @@ export const Header = () => {
 
         }
     }, [isFinish])
-    const {t, i18n} = useTranslation();
+    const changeLanguage = (lng) => {
+        i18n.changeLanguage(lng);
+      };
+    
+      const currentLanguage = i18n.language;
+    
 
     return (
         <section className="site-header">
@@ -60,8 +76,8 @@ export const Header = () => {
                 </Link>
                 <section className="navy">
                     <nav className="navbar">
-                        <Link to="/map" className="nav-item" style={{ '--i': 0 }}>Map</Link>
-                        <Link to="/craigslist" className="nav-item" style={{ '--i': 1 }}>Craigslist</Link>
+                        <Link to="/map" className="nav-item" style={{ '--i': 0 }}>{t('header.map')}</Link>
+                        <Link to="/craigslist" className="nav-item" style={{ '--i': 1 }}>{t('header.craigslist')}</Link>
                         <LanguageSwitcher />
                     </nav>
                     <div className="dropdown" ref={dropdownRef}>
@@ -85,27 +101,27 @@ export const Header = () => {
                         <div className={`dropdown-menu dropdown-menu-right rounded-0 ${isDropdownOpen ? 'active' : ''}`}>
                             <div className="social-icons-header">
                                 <FontAwesomeIcon icon={faMap} className="social-mobile" />
-                                <Link to="/map" className="dropdown-item desktop-unactive" onClick={handleDropdownItemClick}>Map</Link>
+                                <Link to="/map" className="dropdown-item desktop-unactive" onClick={handleDropdownItemClick}>{t('header.map')}</Link>
                             </div>
                             <div className="social-icons-header">
                                 <FontAwesomeIcon className="social-mobile" icon={faBars} />
-                                <Link to="/craigslist" className="dropdown-item desktop-unactive" onClick={handleDropdownItemClick}>Craiglist</Link>
+                                <Link to="/craigslist" className="dropdown-item desktop-unactive" onClick={handleDropdownItemClick}>{t('header.craigslist')}</Link>
                             </div>
                             {!isAuthentication && <>
                                 <div className="social-icons-header">
                                     <FontAwesomeIcon icon={faArrowRightToBracket} />
-                                    <Link to="/sign-up?view=login" className="dropdown-item" onClick={handleDropdownItemClick}>Login</Link>
+                                    <Link to="/sign-up?view=login" className="dropdown-item" onClick={handleDropdownItemClick}>{t('header.login')}</Link>
                                 </div>
                                 <div className="social-icons-header">
                                     <FontAwesomeIcon icon={faAddressCard} />
-                                    <Link to="/sign-up?view=register" className="dropdown-item" onClick={handleDropdownItemClick}>Register</Link>
+                                    <Link to="/sign-up?view=register" className="dropdown-item" onClick={handleDropdownItemClick}>{t('header.register')}</Link>
                                 </div>
                             </>}
                             {isAuthentication && <>
                                 <div className="social-icons-header"><FontAwesomeIcon icon={faUser} />
                                     <div className="alert-modal">
                                         <Link to="/profile" className={`dropdown-item ${!finishProfile ? 'incomplete-profile' : ''}`} onClick={handleDropdownItemClick}>
-                                            Profile
+                                        {t('header.profile')}
                                         </Link>
                                         {!finishProfile && (
                                             <span className="warning-icon" onClick={(e) => {
@@ -118,10 +134,26 @@ export const Header = () => {
                                 </div>
 
                                 <div className="social-icons-header"><FontAwesomeIcon icon={faArrowRightFromBracket} />
-                                    <Link to="/logout" className="dropdown-item" onClick={handleDropdownItemClick}>Logout</Link>
+                                    <Link to="/logout" className="dropdown-item" onClick={handleDropdownItemClick}>{t('header.logout')}</Link>
                                 </div>
 
                             </>}
+
+                            <div className="social-icons-header">
+                <FontAwesomeIcon icon={faGlobe} />
+                <div className="dropdown-item">
+                  {currentLanguage !== 'en' && (
+                    <button onClick={() => changeLanguage('en')}>
+                      English
+                    </button>
+                  )}
+                  {currentLanguage !== 'bg' && (
+                    <button onClick={() => changeLanguage('bg')}>
+                      български
+                    </button>
+                  )}
+                </div>
+              </div>
                         </div>
                     </div>
                 </section>
@@ -131,6 +163,3 @@ export const Header = () => {
                 <p>Вашият профил е непълен. Моля, завършете го, за да продължите да използвате всички възможности на платформата.</p>
             </AlertModal>
         </section>
-
-    )
-}
