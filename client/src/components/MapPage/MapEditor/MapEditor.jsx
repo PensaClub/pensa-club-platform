7b88072bf7eea93c43897263f5f55af85d8521d1
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-05-31 14:50:03 +0300

    create GeoJSON component
    create GeoJson component for all regions in Bulgaria
    add Bulgaria_admin_level_6.geojson file with type Multi Polygon and coordinates for all regions
    
    I added logic for styles and mouse event
    install  leaflet-fullscreen

--- client/src/components/MapPage/MapEditor/MapEditor.jsx
+++ client/src/components/MapPage/MapEditor/MapEditor.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import React, { useEffect, useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, GeoJSON, useMapEvents } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import './mapEditor.css'
+import 'leaflet-fullscreen';
+
+import './mapEditor.css';
 import L, { DivIcon, point } from 'leaflet';
 import { Link } from 'react-router-dom';
-import MarkerClusterGroup from 'react-leaflet-cluster'
+import MarkerClusterGroup from 'react-leaflet-cluster';
 
 
-// Задаване на правилните икони за маркерите
 const DefaultIcon = L.icon({
     iconUrl: require('leaflet/dist/images/marker-icon.png'),
     iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
@@ -22,8 +23,8 @@ const createCustomClusterIcon = (cluster) => {
         html: `<div class="cluster-icon">${cluster.getChildCount()}</div>`,
         className: 'custom-marker-cluster',
         iconSize: point(33, 33, true)
-    })
-}
+    });
+};
 L.Marker.prototype.options.icon = DefaultIcon;
 const adsData = [
     {
@@ -41,33 +42,76 @@ const adsData = [
         position: [42.701655682184395, 23.335979128141805]
     },
     {
-        id: 2,
+        id: 3,
         name: "Bobi Iliev",
         description: "Need assistance with grocery shopping.",
         img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqZctoWcCyQuSUlcN9bKPmSD-B8Gyy_mVo5A&s",
         position: [42.68373935990047, 23.33872570987089]
     },
-
 ];
-const position = [42.72991533257769, 24.674647996012656];
+const position = [42.72991533257769, 24.674647996012656]; // начaлнa позиция -задал съм България
 
 export const MapEditor = () => {
+    const [geoJsonData, setGeoJsonData] = useState(null);
+    const [showGeoJSON, setShowGeoJSON] = useState(true);
+
+    useEffect(() => {
+        fetch('/Bulgaria_admin_level_6.geojson')
+            .then(response => response.json())
+            .then(data => {
+                setGeoJsonData(data);
+            })
+            .catch(error => console.error('Failed to load GeoJSON data', error));
+    }, []);
+
+    const normalStyle = {
+        fillColor: '#ffeb3b',
+        weight: 1,
+        opacity: 1,
+        color: '#e91e63',
+        fillOpacity: 0.15,
+    };
+
+    const onEachFeature = (feature, layer) => {
+        layer.on('mouseover', (e) => {
+            if (showGeoJSON) {
+                e.target.setStyle({
+                    weight: 3,
+                    fillOpacity: 0.6,
+                    fillColor: 'var(--green)',
+                    color: 'var(--orange)',
+                });
+                // e.target.bringToFront(); ненужно за момента 
+            }
+        });
+
+        layer.on('mouseout', (e) => {
+            e.target.setStyle(normalStyle);
+        });
+    };
+
+    const MapEvents = () => {
+        useMapEvents({
+            zoomend: (e) => {
+                const zoomLevel = e.target.getZoom();
+                setShowGeoJSON(zoomLevel <= 12); // направил съм го да изключи над този zoom level 
+            }
+        });
+        return null;
+    };
+
     return (
-        // attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        // url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYmVibzEzMTMzIiwiYSI6ImNsd3E4N2Y2MzEzNWsyanFxcXl0c3g3c24ifQ.4JNDXk3JlfP59tzu1stwOw`}
-
-
-        <MapContainer center={position} zoom={8} scrollWheelZoom={false} style={{ height: "70vh", width: "100%" }}>
+        <MapContainer center={position} zoom={7} scrollWheelZoom={false} style={{ height: "70vh", width: "100%" }} fullscreenControl={true}>
             <TileLayer
-                attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url={'https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png'}
-            // url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url={'https://tile.openstreetmap.org/{z}/{x}/{y}.png'}
             />
-            <MarkerClusterGroup 
-            chunkedLoading 
-            iconCreateFunction={createCustomClusterIcon}
-          
-            showCoverageOnHover={false}
+            <MapEvents />
+            <MarkerClusterGroup
+                chunkedLoading
+                iconCreateFunction={createCustomClusterIcon}
+                showCoverageOnHover={false}
+                
             >
                 {adsData.map(ad => (
                     <Marker key={ad.id} position={ad.position}>
@@ -82,10 +126,11 @@ export const MapEditor = () => {
                             </div>
                         </Popup>
                     </Marker>
-
                 ))}
-            </MarkerClusterGroup >
-
+            </MarkerClusterGroup>
+            {showGeoJSON && geoJsonData && (
+                <GeoJSON data={geoJsonData} style={normalStyle} onEachFeature={onEachFeature} />
+            )}
         </MapContainer>
     );
 };
