74b5acff05b120853d512f9a260e9436cb79e6ee
Nik-Kolev <141948860+Nik-Kolev@users.noreply.github.com>
2024-06-02 18:41:27 +0300

    Enhanced error handling and updated model validations
    - ErrorHandler:
      - Enhanced error handling for unique constraint violations to provide more specific feedback.
    
    - User Details Migration:
      - Updated `username` to support Cyrillic characters.
      - Modified `first_name` and `last_name` fields to accept names of length 3-20 chars and include Cyrillic characters.
      - Changed `work`, `hobbies`, and `interest` fields to accept an array of strings.
      - Updated `district` field to not be required.
    
    - User Details Model:
      - Aligned model changes with migration updates, ensuring consistency across username, first_name, last_name, work, hobbies, interest, and district fields.
    
    - User Details Controller:
      - Implemented enhanced regex (`nameRegex`) to validate names including Cyrillic characters.
      - Updated `usernameRegex` to accommodate Cyrillic characters.
      - Added checks to ensure `work`, `hobby`, and `interest` fields are arrays.
      - Included regex tests for `first_name` and `last_name` during user creation to ensure validation.
      - Updated the creation logic to incorporate `first_name` and `last_name` validations.

--- server/src/sequelize/migrations/20240525092816-create-user-details.js
+++ server/src/sequelize/migrations/20240525092816-create-user-details.js
@@ -1,13 +1,13 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, DataTypes) {
-    await queryInterface.createTable('user_details', {
+    await queryInterface.createTable("user_details", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: DataTypes.INTEGER
+        type: DataTypes.INTEGER,
       },
       phone_number: {
         type: DataTypes.STRING(16),
@@ -15,65 +15,65 @@ module.exports = {
         allowNull: false,
         validate: {
           notEmpty: {
-            msg: 'Phone number cannot be empty.'
+            msg: "Phone number cannot be empty.",
           },
           len: {
             args: [8, 16],
-            msg: 'Phone number has invalid number of characters.'
+            msg: "Phone number has invalid number of characters.",
           },
           is: {
             args: /^(?:\+\d{7,15}|\d{10})$/,
-            msg: 'Phone number must be a valid format.'
+            msg: "Phone number must be a valid format.",
           },
-        }
+        },
       },
       username: {
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
           notEmpty: {
-            msg: 'Username cannot be empty.'
+            msg: "Username cannot be empty.",
           },
           len: {
             args: [6, 16],
-            msg: 'Username must be between 6 and 16 characters.'
+            msg: "Username must be between 6 and 16 characters.",
           },
           is: {
-            args: /^[a-zA-Z][a-zA-Z0-9_]*$/,
-            msg: 'Username must start with a letter and can only contain letters, numbers, and underscores.'
-          }
-        }
+            args: /^[a-zA-Zа-яА-Я][a-zA-Zа-яА-Я0-9_]{6,16}$/,
+            msg: "Username must start with a letter and can only contain letters, numbers, and underscores.",
+          },
+        },
       },
       first_name: {
         type: DataTypes.STRING(20),
         validate: {
           len: {
-            args: [1, 20],
-            msg: 'First name must be between 1 and 20 characters in length.',
+            args: [3, 20],
+            msg: "First name must be between 3 and 20 characters in length.",
           },
           is: {
-            args: /^[a-zA-Z]+$/i,
-            msg: 'First name can only contain letters.',
+            args: /^[a-zA-Zа-яА-Я]{3,20}$/i,
+            msg: "First name must be 3-20 chars, using letters, numbers, or underscores, and include both Cyrillic or Latin alphabets.",
           },
           notEmpty: {
-            msg: 'First name cannot be empty.'
-          }
+            msg: "First name cannot be empty.",
+          },
         },
       },
       last_name: {
-        type: DataTypes.STRING(30),
+        type: DataTypes.STRING(20),
         validate: {
           len: {
-            args: [1, 30],
-            msg: 'Last name must be between 1 and 30 characters in length.',
+            args: [3, 20],
+            msg: "Last name must be between 3 and 20 characters in length.",
           },
           is: {
-            args: /^[a-zA-Z]+$/i,
-            msg: 'Last name can only contain letters.',
+            args: /^[a-zA-Zа-яА-Я]{3,20}$/i,
+            msg: "Last name must be 3-20 chars, using letters, numbers, or underscores, and include both Cyrillic or Latin alphabets.",
           },
           notEmpty: {
-            msg: 'Last name cannot be empty.'
-          }
+            msg: "Last name cannot be empty.",
+          },
         },
       },
       region: {
@@ -81,75 +81,75 @@ module.exports = {
         allowNull: false,
         validate: {
           notEmpty: {
-            msg: 'Region cannot be empty.'
-          }
-        }
+            msg: "Region cannot be empty.",
+          },
+        },
       },
       municipality: {
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
           notEmpty: {
-            msg: 'Municipality cannot be empty.'
-          }
-        }
+            msg: "Municipality cannot be empty.",
+          },
+        },
       },
       settlement: {
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
           notEmpty: {
-            msg: 'Settlement cannot be empty.'
-          }
-        }
+            msg: "Settlement cannot be empty.",
+          },
+        },
       },
       work: {
-        type: DataTypes.STRING,
-        allowNull: false,
+        type: DataTypes.ARRAY(DataTypes.STRING),
+        allowNull: true,
         validate: {
-          notEmpty: {
-            msg: 'Work cannot be empty.'
-          }
-        }
+          isArray(value) {
+            if (value !== null && !Array.isArray(value)) {
+              throw new Error("Work must be an array of strings.");
+            }
+          },
+        },
       },
       hobby: {
-        type: DataTypes.STRING,
-        allowNull: false,
+        type: DataTypes.ARRAY(DataTypes.STRING),
+        allowNull: true,
         validate: {
-          notEmpty: {
-            msg: 'Hobby cannot be empty.'
-          }
-        }
+          isArray(value) {
+            if (value !== null && !Array.isArray(value)) {
+              throw new Error("Hobby must be an array of strings.");
+            }
+          },
+        },
       },
       interest: {
-        type: DataTypes.STRING,
-        allowNull: false,
+        type: DataTypes.ARRAY(DataTypes.STRING),
+        allowNull: true,
         validate: {
-          notEmpty: {
-            msg: 'Interest cannot be empty.'
-          }
-        }
+          isArray(value) {
+            if (value !== null && !Array.isArray(value)) {
+              throw new Error("Interest must be an array of strings.");
+            }
+          },
+        },
       },
       district: {
         type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          notEmpty: {
-            msg: 'District cannot be empty.'
-          }
-        }
       },
       block: {
-        type: DataTypes.STRING
+        type: DataTypes.STRING,
       },
       street: {
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
           notEmpty: {
-            msg: 'Street cannot be empty.'
-          }
-        }
+            msg: "Street cannot be empty.",
+          },
+        },
       },
       street_number: {
         type: DataTypes.STRING,
@@ -159,29 +159,29 @@ module.exports = {
         allowNull: false,
         validate: {
           notEmpty: {
-            msg: 'Location cannot be empty.'
-          }
-        }
+            msg: "Location cannot be empty.",
+          },
+        },
       },
       user_accounts_id: {
         type: DataTypes.INTEGER,
         allowNull: false,
         references: {
-          model: 'user_accounts',
-          key: 'id'
+          model: "user_accounts",
+          key: "id",
         },
       },
       createdAt: {
         allowNull: false,
-        type: DataTypes.DATE
+        type: DataTypes.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: DataTypes.DATE
-      }
+        type: DataTypes.DATE,
+      },
     });
   },
   async down(queryInterface, DataTypes) {
-    await queryInterface.dropTable('user_details');
-  }
+    await queryInterface.dropTable("user_details");
+  },
 };
