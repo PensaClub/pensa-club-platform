fdd352ff1766695a3c62ccedd57a01f6931ed36c
SimonaGancheva <ilievas95@gmail.com>
2024-06-04 15:05:50 +0300

    Translate hero and motto section

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -9,25 +9,27 @@ import {
   faAddressCard,
   faUser,
   faArrowRightFromBracket,
-  faMap, 
-  faBars, faCircleQuestion, faCircleExclamation,
+  faMap,
+  faBars,
+  faCircleQuestion,
+  faCircleExclamation,
   faGlobe,
 } from '@fortawesome/free-solid-svg-icons';
 
 import { useTranslation } from 'react-i18next';
-import AlertModal from "./AlertModal/AlertModal";
+import AlertModal from './AlertModal/AlertModal';
 
 export const Header = () => {
   const { t, i18n } = useTranslation();
 
   const [isMenuOpen, setMenuOpen] = useState(false);
   const [isDropdownOpen, setDropdownOpen] = useState(false);
-    const [isModalOpen, setModalOpen] = useState(false);
-    const { isAuthentication, isFinish } = useContext(UserContext)
-    const [finishProfile, setFinishProfile] = useState(false);
+  const [isModalOpen, setModalOpen] = useState(false);
+  const { isAuthentication, isFinish } = useContext(UserContext);
+  const [finishProfile, setFinishProfile] = useState(false);
   const [currLang, setCurrLang] = useState('en');
 
-    const dropdownRef = useRef(null)
+  const dropdownRef = useRef(null);
 
   const handleClickOutside = (event) => {
     if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
@@ -40,106 +42,166 @@ export const Header = () => {
     setMenuOpen(!isMenuOpen);
   };
 
-    const handleDropdownItemClick = () => {
-        setDropdownOpen(false);
-        setMenuOpen(!isMenuOpen)
+  const handleDropdownItemClick = () => {
+    setDropdownOpen(false);
+    setMenuOpen(!isMenuOpen);
+  };
+
+  const handleModalToggle = () => {
+    setModalOpen(!isModalOpen);
+  };
+
+  useEffect(() => {
+    setFinishProfile(isFinish);
+
+    document.addEventListener('mousedown', handleClickOutside);
+    setMenuOpen(!isMenuOpen);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+      setMenuOpen(!isMenuOpen);
     };
+  }, [isFinish]);
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  };
 
-    const handleModalToggle = () => {
-        setModalOpen(!isModalOpen);
-    };
+  const currentLanguage = i18n.language;
 
-    useEffect(() => {
+  return (
+    <section className="site-header">
+      <header className={isMenuOpen ? 'scrolled' : ''}>
+        <Link to="/">
+          <img src="/images/homePage/logo.png" alt="logo" className="logo" />{' '}
+          Pensa Club
+        </Link>
+        <section className="navy">
+          <nav className="navbar">
+            <Link to="/map" className="nav-item" style={{ '--i': 0 }}>
+              {t('header.map')}
+            </Link>
+            <Link to="/craigslist" className="nav-item" style={{ '--i': 1 }}>
+              {t('header.craigslist')}
+            </Link>
+            
+          </nav>
+          <div className="dropdown" ref={dropdownRef}>
+            <input
+              type="checkbox"
+              id="dropdown-toggle"
+              className="dropdown-checkbox"
+              checked={isDropdownOpen}
+              onChange={handleDropdownToggle}
+            />
+            {!finishProfile && !isDropdownOpen && (
+              <span
+                className="warning-icon-image"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  handleModalToggle();
+                }}
+              >
+                <FontAwesomeIcon icon={faCircleExclamation} />
+              </span>
+            )}
+            <label htmlFor="dropdown-toggle" className="dropdown-toggle">
+              <img
+                src="/images/homePage/avatar2.png"
+                alt="Profile"
+                className="profile-img"
+              />
+            </label>
 
-        setFinishProfile(isFinish)
-
-        document.addEventListener('mousedown', handleClickOutside)
-        setMenuOpen(!isMenuOpen)
-        return () => {
-            document.removeEventListener('mousedown', handleClickOutside)
-            setMenuOpen(!isMenuOpen)
-
-        }
-    }, [isFinish])
-    const changeLanguage = (lng) => {
-        i18n.changeLanguage(lng);
-      };
-    
-      const currentLanguage = i18n.language;
-    
-
-    return (
-        <section className="site-header">
-            <header className={isMenuOpen ? 'scrolled' : ''}>
-                <Link to="/">
-                    <img src='/images/homePage/logo.png' alt="logo" className="logo" /> Pensa Club
+            <div
+              className={`dropdown-menu dropdown-menu-right rounded-0 ${
+                isDropdownOpen ? 'active' : ''
+              }`}
+            >
+              <div className="social-icons-header">
+                <FontAwesomeIcon icon={faMap} className="social-mobile" />
+                <Link
+                  to="/map"
+                  className="dropdown-item desktop-unactive"
+                  onClick={handleDropdownItemClick}
+                >
+                  {t('header.map')}
                 </Link>
-                <section className="navy">
-                    <nav className="navbar">
-                        <Link to="/map" className="nav-item" style={{ '--i': 0 }}>{t('header.map')}</Link>
-                        <Link to="/craigslist" className="nav-item" style={{ '--i': 1 }}>{t('header.craigslist')}</Link>
-                        <LanguageSwitcher />
-                    </nav>
-                    <div className="dropdown" ref={dropdownRef}>
-                        <input
-                            type="checkbox"
-                            id="dropdown-toggle"
-                            className="dropdown-checkbox"
-                            checked={isDropdownOpen}
-                            onChange={handleDropdownToggle}
-                        />
-                        {!finishProfile && !isDropdownOpen && (
-                            <span className="warning-icon-image" onClick={(e) => {
-                                e.stopPropagation();
-                                handleModalToggle();
-                            }}><FontAwesomeIcon icon={faCircleExclamation} /></span>
-                        )}
-                        <label htmlFor="dropdown-toggle" className="dropdown-toggle">
-                            <img src="/images/homePage/avatar2.png" alt="Profile" className="profile-img" />
-                        </label>
+              </div>
+              <div className="social-icons-header">
+                <FontAwesomeIcon className="social-mobile" icon={faBars} />
+                <Link
+                  to="/craigslist"
+                  className="dropdown-item desktop-unactive"
+                  onClick={handleDropdownItemClick}
+                >
+                  {t('header.craigslist')}
+                </Link>
+              </div>
+              {!isAuthentication && (
+                <>
+                  <div className="social-icons-header">
+                    <FontAwesomeIcon icon={faArrowRightToBracket} />
+                    <Link
+                      to="/sign-up?view=login"
+                      className="dropdown-item"
+                      onClick={handleDropdownItemClick}
+                    >
+                      {t('header.login')}
+                    </Link>
+                  </div>
+                  <div className="social-icons-header">
+                    <FontAwesomeIcon icon={faAddressCard} />
+                    <Link
+                      to="/sign-up?view=register"
+                      className="dropdown-item"
+                      onClick={handleDropdownItemClick}
+                    >
+                      {t('header.register')}
+                    </Link>
+                  </div>
+                </>
+              )}
+              {isAuthentication && (
+                <>
+                  <div className="social-icons-header">
+                    <FontAwesomeIcon icon={faUser} />
+                    <div className="alert-modal">
+                      <Link
+                        to="/profile"
+                        className={`dropdown-item ${
+                          !finishProfile ? 'incomplete-profile' : ''
+                        }`}
+                        onClick={handleDropdownItemClick}
+                      >
+                        {t('header.profile')}
+                      </Link>
+                      {!finishProfile && (
+                        <span
+                          className="warning-icon"
+                          onClick={(e) => {
+                            e.stopPropagation();
+                            handleModalToggle();
+                          }}
+                        >
+                          <FontAwesomeIcon icon={faCircleExclamation} />
+                        </span>
+                      )}
+                    </div>
+                  </div>
 
-                        <div className={`dropdown-menu dropdown-menu-right rounded-0 ${isDropdownOpen ? 'active' : ''}`}>
-                            <div className="social-icons-header">
-                                <FontAwesomeIcon icon={faMap} className="social-mobile" />
-                                <Link to="/map" className="dropdown-item desktop-unactive" onClick={handleDropdownItemClick}>{t('header.map')}</Link>
-                            </div>
-                            <div className="social-icons-header">
-                                <FontAwesomeIcon className="social-mobile" icon={faBars} />
-                                <Link to="/craigslist" className="dropdown-item desktop-unactive" onClick={handleDropdownItemClick}>{t('header.craigslist')}</Link>
-                            </div>
-                            {!isAuthentication && <>
-                                <div className="social-icons-header">
-                                    <FontAwesomeIcon icon={faArrowRightToBracket} />
-                                    <Link to="/sign-up?view=login" className="dropdown-item" onClick={handleDropdownItemClick}>{t('header.login')}</Link>
-                                </div>
-                                <div className="social-icons-header">
-                                    <FontAwesomeIcon icon={faAddressCard} />
-                                    <Link to="/sign-up?view=register" className="dropdown-item" onClick={handleDropdownItemClick}>{t('header.register')}</Link>
-                                </div>
-                            </>}
-                            {isAuthentication && <>
-                                <div className="social-icons-header"><FontAwesomeIcon icon={faUser} />
-                                    <div className="alert-modal">
-                                        <Link to="/profile" className={`dropdown-item ${!finishProfile ? 'incomplete-profile' : ''}`} onClick={handleDropdownItemClick}>
-                                        {t('header.profile')}
-                                        </Link>
-                                        {!finishProfile && (
-                                            <span className="warning-icon" onClick={(e) => {
-                                                e.stopPropagation();
-                                                handleModalToggle();
-                                            }}><FontAwesomeIcon icon={faCircleExclamation} /></span>
-                                        )}
-                                    </div>
+                  <div className="social-icons-header">
+                    <FontAwesomeIcon icon={faArrowRightFromBracket} />
+                    <Link
+                      to="/logout"
+                      className="dropdown-item"
+                      onClick={handleDropdownItemClick}
+                    >
+                      {t('header.logout')}
+                    </Link>
+                  </div>
+                </>
+              )}
 
-                                </div>
-
-                                <div className="social-icons-header"><FontAwesomeIcon icon={faArrowRightFromBracket} />
-                                    <Link to="/logout" className="dropdown-item" onClick={handleDropdownItemClick}>{t('header.logout')}</Link>
-                                </div>
-
-                            </>}
-
-                            <div className="social-icons-header">
+              <div className="social-icons-header">
                 <FontAwesomeIcon icon={faGlobe} />
                 <div className="dropdown-item">
                   {currentLanguage !== 'en' && (
@@ -154,12 +216,17 @@ export const Header = () => {
                   )}
                 </div>
               </div>
-                        </div>
-                    </div>
-                </section>
-            </header>
-            <div className="after-header"></div>
-            <AlertModal isOpen={isModalOpen} onClose={handleModalToggle}>
-                <p>Вашият профил е непълен. Моля, завършете го, за да продължите да използвате всички възможности на платформата.</p>
-            </AlertModal>
+            </div>
+          </div>
         </section>
+      </header>
+      <div className="after-header"></div>
+      <AlertModal isOpen={isModalOpen} onClose={handleModalToggle}>
+        <p>
+          Вашият профил е непълен. Моля, завършете го, за да продължите да
+          използвате всички възможности на платформата.
+        </p>
+      </AlertModal>
+    </section>
+  );
+};
--- client/src/components/Home/MottoSection/MottoSection.jsx
+++ client/src/components/Home/MottoSection/MottoSection.jsx
@@ -1,9 +1,7 @@
-import { Link } from 'react-router-dom'
-
 import './motto.css'
 
 import {Link} from 'react-router-dom'
-import { useTranslation } from "react-i18next";
+import { useTranslation, Trans } from "react-i18next";
 
 
 export const MottoSection = () => {
@@ -15,10 +13,10 @@ export const MottoSection = () => {
                 <div className="motto-desc">
                     <img src="/images/homePage/logo.png" alt="logo" />
                     <div className="motto-info">
-                        <h3>ДА ПРЕЧУПИМ ТАЗИ ТЕНДЕНЦИЯ </h3>
-                        <h3>ДА СМЕНИМ ТОЗИ МОДЕЛ </h3>
-                        <h3>ДА БЪДЕМ В КРАК С ВРЕМЕТО  </h3>
-                        <p> Създаване на иновативна, дигитална и <span>интерактивна платформа</span>, насочена към подобряване на качеството на живот на възрастните хора, като ги включи активно в съвременното общество, подкрепи ги в развитието на нови умения и интереси и насърчи <span>здравословния</span> и <span>пълноценен</span>, достоен живот.</p>
+                        <h3>{t('motto.title1')} </h3>
+                        <h3>{t('motto.title2')}</h3>
+                        <h3>{t('motto.title3')}  </h3>
+                        <p> <Trans i18nKey="motto.desc" components={{ span: <strong />}} /></p>
                     </div>
                     <Link to="/#"className="btn-general btn-orange" id="btn-join">{t('motto.about-btn')}</Link> {/*трябва да води към about page*/}
                 </div>
