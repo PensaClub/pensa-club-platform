5c1b8a94cddfe96d73faa16a6bcc1bddce749913
Nik-Kolev <141948860+Nik-Kolev@users.noreply.github.com>
2024-06-05 19:13:44 +0300

    Enhance user details handling and validation
    Utils:
    - Added `ageCalculate` function to compute user age from birthdate.
    
    User Details Model & Migration:
    - Updated regex for first_name and last_name to allow hyphens; set default as null.
    - Renamed 'work/hobby/interest' to 'work_options/skills/interest_options'.
    - Set default values for 'district/block' and 'street_number' as null.
    - Introduced new fields: 'gender' and 'birth_date'.
    
    UserDetailsController:
    - Removed 'isAuth' middleware from 'all-users' endpoint to streamline access.
    - Implemented validation for new 'gender' and 'birth_date' fields; updated field names to match model changes.

--- server/src/controllers/userDetailsController.js
+++ server/src/controllers/userDetailsController.js
@@ -3,20 +3,38 @@ const userDetailsController = require("express").Router();
 const CustomError = require("../utils/customError");
 const { user_details, user_account } = require("../sequelize/models/index");
 const geoCoder = require("../utils/geoCoder");
+const ageCalculate = require("../utils/ageCalculate");
 const isAuth = require("../middlewares/isAuth.js");
 const { where } = require("sequelize");
 
-const notRequiredFields = ["block", "streetNumber", "firstName", "lastName", "district", "work"];
+const notRequiredFields = ["block", "streetNumber", "firstName", "lastName", "district", "work", "gender", "birthDate"];
 
 const usernameRegex = /^[a-zA-Zа-яА-Я][a-zA-Zа-яА-Я0-9_]{6,16}$/;
 const phoneRegex = /^(?:\+\d{7,15}|\d{10})$/;
-const nameRegex = /^[a-zA-Zа-яА-Я]{3,20}$/i;
+const nameRegex = /^[a-zA-Zа-яА-Я0-9_]+(-[a-zA-Zа-яА-Я0-9_]+)*$/i;
+const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
 
 userDetailsController.post("/details", isAuth, async (req, res, next) => {
   let errors = {};
   try {
-    const { region, municipality, settlement, district, block, street, streetNumber, phoneNumber, username, work, hobby, interest, firstName, lastName } =
-      req.body;
+    const {
+      region,
+      municipality,
+      settlement,
+      district,
+      block,
+      street,
+      streetNumber,
+      phoneNumber,
+      username,
+      workOptions,
+      skills, // da e skills - hobby
+      interestOptions,
+      firstName,
+      lastName,
+      gender,
+      birthDate,
+    } = req.body;
 
     Object.entries(req.body).forEach(([fieldName, value]) => {
       if (value === "" && !notRequiredFields.includes(fieldName)) {
@@ -25,7 +43,7 @@ userDetailsController.post("/details", isAuth, async (req, res, next) => {
       }
     });
 
-    ["work", "hobby", "interest"].forEach((field) => {
+    ["workOptions", "skills", "interestOptions"].forEach((field) => {
       if (req.body[field] !== undefined && !Array.isArray(req.body[field])) {
         errors[field] = `${field.charAt(0).toUpperCase() + field.slice(1)} must be an array.`;
       }
@@ -51,6 +69,14 @@ userDetailsController.post("/details", isAuth, async (req, res, next) => {
       errors.lastName = "Last name must be 3-20 chars, using letters, numbers, or underscores, and include both Cyrillic or Latin alphabets.";
     }
 
+    if (gender && (gender !== "male" || gender !== "female" || gender !== "other")) {
+      errors.gender = "Gender must be 'male', 'female', or 'other'.";
+    }
+
+    if (birthDate && (!dateRegex.test(birthDate) || new Date(birthDate) > new Date() || isNaN(new Date(birthDate).getTime()))) {
+      errors.birthDate = "Date format must be YYYY-MM-DD and cannot be in the future.";
+    }
+
     if (Object.keys(errors).length > 0) {
       throw new CustomError({ message: "Validation errors", statusCode: 400, details: errors });
     }
@@ -63,9 +89,9 @@ userDetailsController.post("/details", isAuth, async (req, res, next) => {
       region,
       municipality,
       settlement,
-      work,
-      hobby,
-      interest,
+      work_options: workOptions,
+      skills,
+      interest_options: interestOptions,
       district,
       block,
       street,
@@ -73,21 +99,25 @@ userDetailsController.post("/details", isAuth, async (req, res, next) => {
       location,
       first_name: firstName,
       last_name: lastName,
+      gender,
+      birth_date: birthDate,
       user_accounts_id: req.user.userId,
     };
 
     const details = await user_details.create(data);
+    const { birth_date, ...restOfDetails } = details.dataValues;
 
     await user_account.update({ finished: true }, { where: { email: req.user.email }, returning: true, plain: true });
 
-    const updatedDetails = { ...details.dataValues, enabled: true };
+    const updatedDetails = { ...restOfDetails, age: ageCalculate(birth_date), enabled: true };
+
     res.status(200).send({ message: "Details successfully updated!", details: updatedDetails });
   } catch (err) {
     next(err);
   }
 });
 
-userDetailsController.get("/all-users", isAuth, async (req, res, next) => {
+userDetailsController.get("/all-users", async (req, res, next) => {
   try {
     const accounts = await user_account.findAll({
       attributes: ["id", "email", ["finished", "enabled"]],
