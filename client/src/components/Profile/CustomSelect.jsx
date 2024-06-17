19e8bc794b5736d2be4ee3fe5d336ff5a7065d35
Dafy Yasenova <dafy_yasenova@abv.bg>
2024-06-17 16:19:27 +0300

    Update CustomSelect options with counter, selectAll and clear buttons

--- client/src/components/Profile/CustomSelect.jsx
+++ client/src/components/Profile/CustomSelect.jsx
@@ -1,22 +1,26 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons';
-import './CustomSelect.css'; // Ensure to create and import your CSS for styling
+import './CustomSelect.css';
 
-const CustomSelect = ({ options, selectedOptions, onSelect, icon }) => {
+const CustomSelect = ({ options, selectedOptions, onSelect}) => {
     const [isOpen, setIsOpen] = useState(false);
     const dropdownRef = useRef(null);
 
-    const handleToggleDropdown = () => {
+    const handleToggleDropdown = (e) => {
+        e.preventDefault();
         setIsOpen(!isOpen);
     };
 
     const handleCheckboxChange = (e) => {
+        e.stopPropagation();
+
         const value = e.target.value;
+
         const newSelectedOptions = selectedOptions.includes(value)
             ? selectedOptions.filter(option => option !== value)
             : [...selectedOptions, value];
-        
+
         onSelect(newSelectedOptions);
     };
 
@@ -25,6 +29,20 @@ const CustomSelect = ({ options, selectedOptions, onSelect, icon }) => {
             setIsOpen(false);
         }
     };
+
+
+    const handleSelectAll = (e) => {
+        e.preventDefault()
+        const allOptions = options.map(option => option.value);
+        onSelect(allOptions);
+    };
+
+    const handleClearSelection = (e) => {
+        e.preventDefault()
+        onSelect([]);
+    };
+
+
 
     useEffect(() => {
         document.addEventListener('mousedown', handleClickOutside);
@@ -35,20 +53,25 @@ const CustomSelect = ({ options, selectedOptions, onSelect, icon }) => {
 
     return (
         <div className="custom-select-with-checkbox" ref={dropdownRef}>
-            <label className="custom-select-label">
-                {icon && <span className="custom-select-icon"><FontAwesomeIcon icon={icon} /></span>}
-                
-            </label>
+            <label className="custom-select-label"></label>
             <div className="custom-select-header" onClick={handleToggleDropdown}>
-                <span>{selectedOptions.length > 0 ? selectedOptions.join(', ') : 'Изберете'}</span>
+                <span>{selectedOptions.length > 0 ? `${selectedOptions.length} избрани ` : 'Изберете'}</span>
                 <span className="custom-select-chevron">
                     <FontAwesomeIcon icon={isOpen ? faChevronUp : faChevronDown} />
                 </span>
             </div>
             {isOpen && (
-                <div className="custom-select-dropdown">
+                <div className="custom-select-dropdown" onClick={(e) => e.stopPropagation()}>
+
+                    <div className="select-all-clear">
+                        <button onClick={handleSelectAll} className="select-all-btn">Изберете всички</button>
+                        <button onClick={handleClearSelection} className="clear-btn">Изчистете</button>
+                    </div>
+
                     {options.map(option => (
-                        <div key={option.value} className="checkbox-item">
+
+                        <div key={option.value} className={`checkbox-item ${selectedOptions.includes(option.value) ? 'checked' : ''}`}>
+
                             <label>
                                 <input
                                     type="checkbox"
