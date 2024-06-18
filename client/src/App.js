c269356b4cbb55de4ca55529109a3d31b8757e0e
Kristiyan <kristiyan.konov@forkpoint.com>
2024-06-19 00:23:16 +0300

    Fix wring imports

--- client/src/App.js
+++ client/src/App.js
@@ -15,12 +15,7 @@ import {FiltersMap} from './components/MapPage/FitlersMap/FiltersMap';
 import { MapPage } from './components/MapPage/MapPage';
 import { PublicGuard } from './components/Guards/PublicGuard.jsx';
 import { AuthGuard } from './components/Guards/AuthGuard.jsx';
-import { MapProvider } from './components/contexts/mapContext.jsx';
-
-
-
-
-
+import { MapProvider } from './components/contexts/MapContext.jsx';
 
 function App() {
 
