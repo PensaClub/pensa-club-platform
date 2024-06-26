d52e42965431baac9d05fb3758210d00be9121c5
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-06-26 20:08:31 +0300

    Added Masonry layout and loading skeletons to community filters
    Implemented Masonry layout in community filters for improved responsive design. The layout now adjusts based on the screen size, improving UX. Moreover, added loading skeletons to give a more interactive and smooth experience during data fetch.
    
    Styles of elements were updated, including changing footer position styles and refined visual elements in filter options. The code for handling open states for filter options is refined resulting in much cleaner and shorter code.
    
    No breaking changes were introduced with these revisions, and the application's overall functionality remains intact.
    
    Lastly, minor cleanups in CSS files were also performed, including removing unnecessary whitespace and comments.
    
    This changes contribute to a more dynamic UI and better user experience for slower network conditions.  No associated issue.

--- client/src/components/Community/FiltersCommunity/FiltersCommunity.jsx
+++ client/src/components/Community/FiltersCommunity/FiltersCommunity.jsx
@@ -1,11 +1,14 @@
+import React, { useState, useEffect } from 'react';
 import { useCommunityContext } from "../../contexts/CommunityContext";
-import { useEffect, useState } from "react";
+import Masonry from 'react-masonry-css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 import './filtersCommunity.css';
 
 export const FiltersCommunity = () => {
-    const { fetchSubregions, regions, subregions } = useCommunityContext();
+    const { fetchSubregions, regions, subregions, isLoading } = useCommunityContext();
     const [openSelects, setOpenSelects] = useState({});
     const [selectedSubregions, setSelectedSubregions] = useState({});
 
@@ -27,42 +30,60 @@ export const FiltersCommunity = () => {
         setSelectedSubregions(prev => ({ ...prev, [regionId]: subregionId }));
     };
 
+    const breakpointColumnsObj = {
+        default: 2, //когато ширината на екрана е 768 пиксела или по-голяма , ще се използва 2 колони.
+        768: 1 // когато ширината на екрана е 768 пиксела или по-малка, ще се използва 1 колона.
+    };
+
     return (
         <>
             <section className="unique-main-filters">
-                <div className="unique-filters-community">
-                    {regions.map(region => (
-                        <div key={region.id} className={`unique-region-filter ${openSelects[region.id] ? 'open' : ''}`}>
-                            <div className="unique-select-container">
-                                <div className="unique-select-wrapper">
-                                    <div
-                                        id={`select-${region.id}`}
-                                        className={`unique-select-display ${openSelects[region.id] ? 'open' : ''}`}
-                                        onClick={() => toggleSelect(region.id)}
-                                    >
-                                        {region.bg}
-                                    </div>
-                                    <FontAwesomeIcon
-                                        icon={openSelects[region.id] ? faChevronUp : faChevronDown}
-                                        className={`unique-select-arrow ${openSelects[region.id] ? 'open' : ''}`}
-                                        onClick={() => toggleSelect(region.id)}
-                                    />
-                                </div>
-                                <div className={`unique-options-container ${openSelects[region.id] ? 'open' : ''}`}>
-                                    {subregions[region.id] && subregions[region.id].map(subregion => (
+                <Masonry
+                    breakpointCols={breakpointColumnsObj}
+                    className="my-masonry-grid"
+                    columnClassName="my-masonry-grid_column">
+                    {isLoading ? (
+                        Array(5).fill().map((_, index) => (
+                            <Skeleton key={index} height={60} className="unique-skeleton" />
+                        ))
+                    ) : (
+                        regions.map(region => (
+                            <div key={region.id} className={`unique-region-filter ${openSelects[region.id] ? 'open' : ''}`}>
+                                <div className="unique-select-container">
+                                    <div className="unique-select-wrapper">
                                         <div
-                                            key={subregion.id}
-                                            className={`unique-option ${selectedSubregions[region.id] === subregion.id ? 'selected' : ''}`}
-                                            onClick={() => handleOptionClick(region.id, subregion.id)}
+                                            id={`select-${region.id}`}
+                                            className={`unique-select-display ${openSelects[region.id] ? 'open' : ''}`}
+                                            onClick={() => toggleSelect(region.id)}
                                         >
-                                            {subregion.bg}
+                                            {region.bg}
                                         </div>
-                                    ))}
+                                        <FontAwesomeIcon
+                                            icon={openSelects[region.id] ? faChevronUp : faChevronDown}
+                                            className={`unique-select-arrow ${openSelects[region.id] ? 'open' : ''}`}
+                                            onClick={() => toggleSelect(region.id)}
+                                        />
+                                    </div>
+                                    <div className={`unique-options-container ${openSelects[region.id] ? 'open' : ''}`}>
+                                        {subregions[region.id] ? (
+                                            subregions[region.id].map(subregion => (
+                                                <div
+                                                    key={subregion.id}
+                                                    className={`unique-option ${selectedSubregions[region.id] === subregion.id ? 'selected' : ''}`}
+                                                    onClick={() => handleOptionClick(region.id, subregion.id)}
+                                                >
+                                                    {subregion.bg}
+                                                </div>
+                                            ))
+                                        ) : (
+                                            <Skeleton count={5} height={40} className="unique-skeleton-option" />
+                                        )}
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    ))}
-                </div>
+                        ))
+                    )}
+                </Masonry>
             </section>
         </>
     );
