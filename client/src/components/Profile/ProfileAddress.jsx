44fbcbbae839b09758e48420c8e64eab2f71e81c
Nik-Kolev <141948860+Nik-Kolev@users.noreply.github.com>
2024-06-28 00:27:31 +0300

    Enhance user profile management and optimize login process
    - Optimize login by reducing server requests with enhanced userController.
    - Update ProfileForm with image upload capabilities, image preview, and code optimizations.
    - Add region.municipality and settlement ID mapping in onProfileDataSubmit and onEditProfileDataSubmit.
    - Fix translation discrepancies and minor issues in Profile Address.
    - Streamline Profile by removing redundant code, fixing merge errors, and removing the change image link.
    - Update header.css with missing image class.
    - Ensure userDetailsController updates new users with imageURL/firebaseImagePath.

--- client/src/components/Profile/ProfileAddress.jsx
+++ client/src/components/Profile/ProfileAddress.jsx
@@ -1,11 +1,7 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import {
-  handleReset,
-  validateField,
-  trimObjectStrings,
-} from '../../utils/profile';
+import { handleReset, validateField, trimObjectStrings } from '../../utils/profile';
 import { UserContext } from '../contexts/UserContext';
 
 import { loadData } from '../../utils/loadData';
@@ -14,8 +10,7 @@ const ProfileAddress = () => {
   const navigate = useNavigate();
   const { t, i18n } = useTranslation();
   const currentLanguage = i18n.language;
-  const { profileData, onEditProfileDataSubmit, addressId } =
-    useContext(UserContext);
+  const { profileData, onEditProfileDataSubmit, addressId } = useContext(UserContext);
 
   const [regions, setRegions] = useState([]);
   const [municipalities, setMunicipalities] = useState([]);
@@ -41,22 +36,18 @@ const ProfileAddress = () => {
       .then((data) => setRegions(data))
       .catch((err) => console.log(err.message));
 
-    loadData(
-      `/regions-data/region-${form.regionId}/subregions-${form.regionId}.json`
-    )
+    loadData(`/regions-data/region-${form.regionId}/subregions-${form.regionId}.json`)
       .then((data) => setMunicipalities(data))
       .catch((err) => console.log(err.message));
 
-    loadData(
-      `/regions-data/region-${form.regionId}/towns/towns-${form.municipalityId}.json`
-    )
+    loadData(`/regions-data/region-${form.regionId}/towns/towns-${form.municipalityId}.json`)
       .then((data) => setSettlements(data))
       .catch((err) => console.log(err.message));
-  }, []);
+  }, [form.municipalityId, form.regionId]);
 
   const handleRegionChange = async (e) => {
     const regionId = e.target.value;
-    const currRegion = regions.filter((region) => region.id == regionId);
+    const currRegion = regions.filter((region) => region.id === regionId);
     const regionName = currRegion[0].bg;
 
     setForm({
@@ -72,9 +63,7 @@ const ProfileAddress = () => {
     setSettlements([]);
 
     try {
-      const response = await fetch(
-        `/regions-data/region-${regionId}/subregions-${regionId}.json`
-      );
+      const response = await fetch(`/regions-data/region-${regionId}/subregions-${regionId}.json`);
 
       const data = await response.json();
 
@@ -86,9 +75,7 @@ const ProfileAddress = () => {
 
   const handleMunicipalityChange = async (e) => {
     const municipalityId = e.target.value;
-    const currMunicipality = municipalities.filter(
-      (municipality) => municipality.id == municipalityId
-    );
+    const currMunicipality = municipalities.filter((municipality) => municipality.id === municipalityId);
     const municipalityName = currMunicipality[0].bg;
 
     setForm({
@@ -101,9 +88,7 @@ const ProfileAddress = () => {
     setSettlements([]);
 
     try {
-      const response = await fetch(
-        `/regions-data/region-${form.regionId}/towns/towns-${municipalityId}.json`
-      );
+      const response = await fetch(`/regions-data/region-${form.regionId}/towns/towns-${municipalityId}.json`);
       const data = await response.json();
       setSettlements(data);
     } catch (error) {
@@ -113,9 +98,7 @@ const ProfileAddress = () => {
 
   const handleSettlementChange = async (e) => {
     const settlementId = e.target.value;
-    const currSettlement = settlements.filter(
-      (settlement) => settlement.id == settlementId
-    );
+    const currSettlement = settlements.filter((settlement) => settlement.id === settlementId);
     const settlementName = currSettlement[0].bg;
 
     setForm({
@@ -126,9 +109,7 @@ const ProfileAddress = () => {
     setSettlements([]);
 
     try {
-      const response = await fetch(
-        `/regions-data/region-${form.regionId}/towns/towns-${form.municipalityId}.json`
-      );
+      const response = await fetch(`/regions-data/region-${form.regionId}/towns/towns-${form.municipalityId}.json`);
       const data = await response.json();
       setSettlements(data);
     } catch (error) {
@@ -185,20 +166,20 @@ const ProfileAddress = () => {
     handleReset(setForm, initialFormState);
   };
   return (
-    <form onSubmit={handleSubmit} className="profile-form">
+    <form onSubmit={handleSubmit} className='profile-form'>
       <h3>{t('profile.address_form')}</h3>
 
       <label>
         {t('profile.region')}: <span>*</span>
         <select
-          name="region"
+          name='region'
           value={form.regionId}
           onChange={handleRegionChange}
           onBlur={onBlurHandler}
           required
           style={{ borderColor: errors.region ? '#BB1D3D' : '' }}
         >
-          <option value="">{t('profile.select_region')}</option>
+          <option value=''>{t('profile.select_region')}</option>
           {regions.map((region, index) => (
             <option key={index} value={region.id}>
               {currentLanguage === 'bg' && `${region.bg}`}
@@ -206,19 +187,19 @@ const ProfileAddress = () => {
             </option>
           ))}
         </select>
-        {errors.region && <span className="error">{errors.region}</span>}
+        {errors.region && <span className='error'>{errors.region}</span>}
       </label>
       <label>
         {t('profile.municipality')}: <span>*</span>
         <select
-          name="municipality"
+          name='municipality'
           value={form.municipalityId}
           onChange={handleMunicipalityChange}
           onBlur={onBlurHandler}
           required
           style={{ borderColor: errors.municipality ? '#BB1D3D' : '' }}
         >
-          <option value="">{t('profile.select_municipality')}</option>
+          <option value=''>{t('profile.select_municipality')}</option>
           {municipalities.map((municipality, index) => (
             <option key={index} value={municipality.id}>
               {currentLanguage === 'bg' && `${municipality.bg}`}
@@ -226,21 +207,19 @@ const ProfileAddress = () => {
             </option>
           ))}
         </select>
-        {errors.municipality && (
-          <span className="error">{errors.municipality}</span>
-        )}
+        {errors.municipality && <span className='error'>{errors.municipality}</span>}
       </label>
       <label>
         {t('profile.settlement')}: <span>*</span>
         <select
-          name="settlement"
+          name='settlement'
           value={form.settlementId}
           onChange={handleSettlementChange}
           onBlur={onBlurHandler}
           required
           style={{ borderColor: errors.settlement ? '#BB1D3D' : '' }}
         >
-          <option value="">{t('profile.select_settlement')}</option>
+          <option value=''>{t('profile.select_settlement')}</option>
           {settlements.map((settlement, index) => (
             <option key={index} value={settlement.id}>
               {currentLanguage === 'bg' && `${settlement.bg}`}
@@ -248,66 +227,48 @@ const ProfileAddress = () => {
             </option>
           ))}
         </select>
-        {errors.settlement && (
-          <span className="error">{errors.settlement}</span>
-        )}
+        {errors.settlement && <span className='error'>{errors.settlement}</span>}
       </label>
       <label>
         {t('profile.district')}:
-        <input
-          type="text"
-          name="district"
-          value={form.district}
-          onChange={handleInputChange}
-        />
-        {errors.district && <span className="error">{errors.district}</span>}
+        <input type='text' name='district' value={form.district} onChange={handleInputChange} />
+        {errors.district && <span className='error'>{errors.district}</span>}
       </label>
       <label>
         {t('profile.block')}:
-        <input
-          type="text"
-          name="block"
-          value={form.block}
-          onChange={handleInputChange}
-        />
-        {errors.block && <span className="error">{errors.block}</span>}
+        <input type='text' name='block' value={form.block} onChange={handleInputChange} />
+        {errors.block && <span className='error'>{errors.block}</span>}
       </label>
       <label>
         {t('profile.street')}: <span>*</span>
         <input
-          type="text"
-          name="street"
+          type='text'
+          name='street'
           value={form.street}
           onChange={handleInputChange}
           onBlur={onBlurHandler}
           required
           style={{ borderColor: errors.street ? '#BB1D3D' : '' }}
         />
-        {errors.street && <span className="error">{errors.street}</span>}
+        {errors.street && <span className='error'>{errors.street}</span>}
       </label>
       <label>
         {t('profile.street_number')}:
         <input
-          type="text"
-          name="streetNumber"
+          type='text'
+          name='streetNumber'
           value={form.streetNumber}
           onChange={handleInputChange}
           onBlur={onBlurHandler}
           style={{ borderColor: errors.streetNumber ? '#BB1D3D' : '' }}
         />
-        {errors.streetNumber && (
-          <span className="error">{errors.streetNumber}</span>
-        )}
+        {errors.streetNumber && <span className='error'>{errors.streetNumber}</span>}
       </label>
-      <span className="required-fields">{t('profile.required_fields')}</span>
-      <button className="btn-general btn-green btn-profile" type="submit">
+      <span className='required-fields'>{t('profile.required_fields')}</span>
+      <button className='btn-general btn-green btn-profile' type='submit'>
         {t('profile.save_btn')}
       </button>
-      <button
-        type="submit"
-        className="btn-general btn-red"
-        onClick={handleResetForm}
-      >
+      <button type='submit' className='btn-general btn-red' onClick={handleResetForm}>
         {t('profile.close_btn')}
       </button>
     </form>
--- server/src/controllers/userDetailsController.js
+++ server/src/controllers/userDetailsController.js
@@ -1,18 +1,18 @@
-const userDetailsController = require("express").Router();
+const userDetailsController = require('express').Router();
 
-const { user_details, user_account } = require("../sequelize/models/index");
-const geoCoder = require("../utils/geoCoder");
-const ageCalculate = require("../utils/ageCalculate");
-const userDetailsValidator = require("../utils/userDetailsValidator");
-const isAuth = require("../middlewares/isAuth.js");
-const { where } = require("sequelize");
-const { tokenCreator } = require("../utils/jwt.js");
-const fieldSwap = require("../utils/fieldSwap.js");
-const memoryCache = require("../middlewares/caching.js");
+const { user_details, user_account } = require('../sequelize/models/index');
+const geoCoder = require('../utils/geoCoder');
+const ageCalculate = require('../utils/ageCalculate');
+const userDetailsValidator = require('../utils/userDetailsValidator');
+const isAuth = require('../middlewares/isAuth.js');
+const { where } = require('sequelize');
+const { tokenCreator } = require('../utils/jwt.js');
+const fieldSwap = require('../utils/fieldSwap.js');
+const memoryCache = require('../middlewares/caching.js');
 
-userDetailsController.post("/details", isAuth, async (req, res, next) => {
+userDetailsController.post('/details', isAuth, async (req, res, next) => {
   if (req.user.enabled) {
-    return res.status(403).send({ message: "User details have already been submitted once." });
+    return res.status(403).send({ message: 'User details have already been submitted once.' });
   }
 
   try {
@@ -34,6 +34,8 @@ userDetailsController.post("/details", isAuth, async (req, res, next) => {
       lastName,
       gender,
       birthDate,
+      imageURL,
+      firebaseImagePath,
     } = req.body;
 
     const location = await geoCoder({ streetNumber, street, district, settlement, municipality, region });
@@ -57,6 +59,8 @@ userDetailsController.post("/details", isAuth, async (req, res, next) => {
       gender,
       birth_date: birthDate,
       user_accounts_id: req.user.userId,
+      imageURL: imageURL,
+      firebase_image_path: firebaseImagePath,
     };
 
     const details = await user_details.create(data);
@@ -67,47 +71,47 @@ userDetailsController.post("/details", isAuth, async (req, res, next) => {
 
     const token = tokenCreator(user[1].dataValues);
 
-    const updatedDetails = { ...fieldSwap(restOfDetails, "mapFromDb"), age: ageCalculate(restOfDetails.birth_date) };
+    const updatedDetails = { ...fieldSwap(restOfDetails, 'mapFromDb'), age: ageCalculate(restOfDetails.birth_date) };
 
-    res.status(200).send({ message: "Details successfully updated!", user: { email: req.user.email, enabled: true, details: updatedDetails }, token });
+    res.status(200).send({ message: 'Details successfully updated!', user: { email: req.user.email, enabled: true, details: updatedDetails }, token });
   } catch (err) {
     next(err);
   }
 });
 
-userDetailsController.get("/all-users", memoryCache, async (req, res, next) => {
+userDetailsController.get('/all-users', memoryCache, async (req, res, next) => {
   try {
     const accounts = await user_account.findAll({
-      attributes: ["email", ["finished", "enabled"]],
+      attributes: ['email', ['finished', 'enabled']],
       include: [
         {
           model: user_details,
-          as: "details",
+          as: 'details',
           attributes: [
-            ["phone_number", "phoneNumber"],
-            "username",
-            ["first_name", "firstName"],
-            ["last_name", "lastName"],
-            ["work_options", "workOptions"],
-            "skills",
-            ["interest_options", "interestOptions"],
-            "location",
+            ['phone_number', 'phoneNumber'],
+            'username',
+            ['first_name', 'firstName'],
+            ['last_name', 'lastName'],
+            ['work_options', 'workOptions'],
+            'skills',
+            ['interest_options', 'interestOptions'],
+            'location',
           ],
         },
       ],
     });
 
-    res.status(200).json({ message: "Users data retrieved successfully.", accounts });
+    res.status(200).json({ message: 'Users data retrieved successfully.', accounts });
   } catch (err) {
     next(err);
   }
 });
 
-userDetailsController.patch("/update-details", isAuth, async (req, res, next) => {
+userDetailsController.patch('/update-details', isAuth, async (req, res, next) => {
   try {
     userDetailsValidator(req.body, req.path);
 
-    const addressUpdate = ["region", "municipality", "settlement", "district", "block", "street", "streetNumber"];
+    const addressUpdate = ['region', 'municipality', 'settlement', 'district', 'block', 'street', 'streetNumber'];
 
     const addressData = {};
 
@@ -117,7 +121,7 @@ userDetailsController.patch("/update-details", isAuth, async (req, res, next) =>
       }
     });
 
-    const data = fieldSwap(req.body, "mapToDb");
+    const data = fieldSwap(req.body, 'mapToDb');
 
     let location;
     if (Object.keys(addressData).length > 0) {
@@ -127,43 +131,43 @@ userDetailsController.patch("/update-details", isAuth, async (req, res, next) =>
 
     const [_, details] = await user_details.update(data, { where: { user_accounts_id: req.user.userId }, returning: true, plain: true });
 
-    const updatedDetails = fieldSwap(details.dataValues, "mapFromDb");
+    const updatedDetails = fieldSwap(details.dataValues, 'mapFromDb');
 
     updatedDetails.age = ageCalculate(updatedDetails.birthDate);
 
-    res.status(200).json({ message: "Details edited successfully!", details: updatedDetails });
+    res.status(200).json({ message: 'Details edited successfully!', details: updatedDetails });
   } catch (err) {
     next(err);
   }
 });
 
-userDetailsController.get("/single-user", isAuth, async (req, res, next) => {
+userDetailsController.get('/single-user', isAuth, async (req, res, next) => {
   if (!req.user.userId) {
-    return res.status(401).json({ message: "Authentication failed. User not found." });
+    return res.status(401).json({ message: 'Authentication failed. User not found.' });
   }
 
   try {
     const user = await user_account.findOne({
       where: { id: req.user.userId },
-      attributes: ["email", ["finished", "enabled"]],
+      attributes: ['email', ['finished', 'enabled']],
       include: [
         {
           model: user_details,
-          as: "details",
-          attributes: { exclude: ["user_accounts_id", "id"] },
+          as: 'details',
+          attributes: { exclude: ['user_accounts_id', 'id'] },
         },
       ],
     });
 
     if (!user) {
-      return res.status(404).json({ message: "User not found." });
+      return res.status(404).json({ message: 'User not found.' });
     }
 
-    const details = fieldSwap(user.dataValues.details.dataValues, "mapFromDb");
+    const details = fieldSwap(user.dataValues.details.dataValues, 'mapFromDb');
 
     details.age = ageCalculate(details.birthDate);
 
-    res.status(200).json({ message: "User data retrieved successfully.", user: { email: user.dataValues.email, enabled: user.dataValues.enabled, details } });
+    res.status(200).json({ message: 'User data retrieved successfully.', user: { email: user.dataValues.email, enabled: user.dataValues.enabled, details } });
   } catch (err) {
     next(err);
   }
