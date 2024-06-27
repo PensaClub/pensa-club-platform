65cb66f1a7be0ec18c8d5de533550dbf75afe7b3
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-06-27 20:47:37 +0300

    fix gap in ads tags

--- client/src/components/Community/AdsCard/AdsCard.jsx
+++ client/src/components/Community/AdsCard/AdsCard.jsx
@@ -1,43 +1,8 @@
 import './adsCard.css';
 
 
-export const AdsCard = ({ cards }) => {
-    const ads =
-        [
-            {
-                "id": 1,
-                "created_date": "2024-06-26",
-                "expiry_date": "2024-07-26",
-                "title": "Продавам велосипед",
-                "type": "продавам",
-                "image": "/community/ads.jpg",
-                "user_name": "Иван Иванов",
-                "city": "София",
-                "item": "Велосипед"
-            },
-            {
-                "id": 2,
-                "created_date": "2024-06-20",
-                "expiry_date": "2024-07-20",
-                "title": "Купувам лаптоп",
-                "type": "купувам",
-                "image": "/community/ads.jpg",
-                "user_name": "Мария Петрова",
-                "city": "Пловдив",
-                "item": "Лаптоп"
-            },
-            {
-                "id": 3,
-                "created_date": "2024-06-15",
-                "expiry_date": "2024-07-15",
-                "title": "Подарявам диван",
-                "type": "Дарявам",
-                "image": "/community/ads.jpg",
-                "user_name": "Георги Георгиев",
-                "city": "Варна",
-                "item": "Диван"
-            }
-        ]
+export const AdsCard = ({ ads }) => {
+   
         return (
             <section className="ads-main">
                 {ads.map(ad => (
--- client/src/components/Community/CommunityPage.jsx
+++ client/src/components/Community/CommunityPage.jsx
@@ -7,6 +7,42 @@ import { CommunityFooter } from "./CommunityFooter/CommunityFooter";
 import { AdsCard } from "./AdsCard/AdsCard";
 
 export const CommunityPage = () => {
+    const ads =
+    [
+        {
+            "id": 1,
+            "created_date": "2024-06-26",
+            "expiry_date": "2024-07-26",
+            "title": "Продавам велосипед",
+            "type": "продавам",
+            "image": "/community/ads.jpg",
+            "user_name": "Иван Иванов",
+            "city": "София",
+            "item": "Велосипед"
+        },
+        {
+            "id": 2,
+            "created_date": "2024-06-20",
+            "expiry_date": "2024-07-20",
+            "title": "Купувам лаптоп",
+            "type": "купувам",
+            "image": "/community/ads.jpg",
+            "user_name": "Мария Петрова",
+            "city": "Пловдив",
+            "item": "Лаптоп"
+        },
+        {
+            "id": 3,
+            "created_date": "2024-06-15",
+            "expiry_date": "2024-07-15",
+            "title": "Подарявам диван",
+            "type": "Дарявам",
+            "image": "/community/ads.jpg",
+            "user_name": "Георги Георгиев",
+            "city": "Варна",
+            "item": "Диван"
+        }
+    ]
     return (
         <>
         <section className="background-community">
@@ -35,8 +71,8 @@ export const CommunityPage = () => {
 
                         </div>
                     </div>
-                    {/* <FiltersCommunity /> */}
-                    <AdsCard/>
+                    {ads.length>0 ?<AdsCard ads={ads}/>: <FiltersCommunity />
+                    }
                 </section>
                 <CommunityFooter />
 
