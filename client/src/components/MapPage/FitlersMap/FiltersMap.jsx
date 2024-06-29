5b1fec460d7cf7920ce1606f3f21b17ad763af40
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-06-29 19:52:54 +0300

    Update FiltersMap.jsx

--- client/src/components/MapPage/FitlersMap/FiltersMap.jsx
+++ client/src/components/MapPage/FitlersMap/FiltersMap.jsx
@@ -176,11 +176,11 @@ export const FiltersMap = () => {
         return allUsers.response.accounts.filter(user => {
             const details = user.details || {};
 
-            const { work_options = [], skills = [], interest_options = [] } = details;
+            const { workOptions = [], skills = [], interestOptions = [] } = details;
 
             const skillsMatch = selectedSkills.length === 0 || selectedSkills.some(skill => skills.includes(skill));
-            const worksMatch = selectedWorks.length === 0 || selectedWorks.some(workOption => work_options.includes(workOption));
-            const interestsMatch = selectedInterests.length === 0 || selectedInterests.some(interest => interest_options.includes(interest));
+            const worksMatch = selectedWorks.length === 0 || selectedWorks.some(workOption => workOptions.includes(workOption));
+            const interestsMatch = selectedInterests.length === 0 || selectedInterests.some(interest => interestOptions.includes(interest));
 
             return skillsMatch && worksMatch && interestsMatch;
         });
