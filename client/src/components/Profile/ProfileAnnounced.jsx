6346f5564316eef8997003c921336301b6a86d73
Dafy Yasenova <dafy_yasenova@abv.bg>
2024-07-07 19:54:38 +0300

    Profile Announced/my Ads

--- client/src/components/Profile/ProfileAnnounced.jsx
+++ client/src/components/Profile/ProfileAnnounced.jsx
@@ -5,32 +5,52 @@ import { faLocationDot, faPhone, faEnvelope, faSheetPlastic } from '@fortawesome
 
 export const ProfileAnnounced = () => {
     return (
-        <div className="announced">
-            <section className="profile-data">
-                <div className="avatar-announced">
+        <>
+        <div className='announced'>
+            <p className='view-more'>виж повече</p>
+            <section className='profile-data ads'>
+                <div className='avatar-announced'>
                     <img src="/images/sign-up/avatar.jpg" alt="User avatar" />
+                    <p>продавам</p>
                 </div>
-                <div className="user-data">
-                    <h2>Обява 1</h2>
-                    <p><FontAwesomeIcon icon={faSheetPlastic} className="icon" /> Име на обява</p>
-                    <p><FontAwesomeIcon icon={faPhone} className="icon" /> +35659599589</p>
-                    <p><FontAwesomeIcon icon={faLocationDot} className="icon" /> гр.София</p>
-                    <p><FontAwesomeIcon icon={faEnvelope} className="icon" /> example@gmail.com</p>
+                <div className='user-data user-data-ads'>
+                    <h3>Обява 1</h3>
+                    <div className='ads-elipse'>
+                    <p className='elipse'>мебели</p>
+                    <p className='elipse'>София</p>
+                    <p className='elipse'>юни </p>
+                    </div>
+                    <p>Валидна до: 01 юни 2025</p>
+                    <div className='ads-btns'>
+                    <button className='ads-btn red'>Редактирай</button>
+                    <button className='ads-btn green'>Изтрий</button>
+                    </div>
                 </div>
             </section>
-
-            <section className="profile-data">
-                <div className="avatar-announced">
+</div>
+            <div className='announced'>
+            <p className='view-more'>виж повече</p>
+            <section className='profile-data ads'>
+                <div className='avatar-announced'>
                     <img src="/images/sign-up/avatar.jpg" alt="User avatar" />
+                    <p>продавам</p>
                 </div>
-                <div className="user-data">
-                    <h2>Обява 2</h2>
-                    <p><FontAwesomeIcon icon={faSheetPlastic} className="icon" /> Име на обява</p>
-                    <p><FontAwesomeIcon icon={faPhone} className="icon" /> +35659599589</p>
-                    <p><FontAwesomeIcon icon={faLocationDot} className="icon" /> гр.София</p>
-                    <p><FontAwesomeIcon icon={faEnvelope} className="icon" /> example@gmail.com</p>
+                <div className='user-data user-data-ads'>
+                    <h3>Обява 1</h3>
+                    <div className='ads-elipse'>
+                    <p className='elipse'>мебели</p>
+                    <p className='elipse'>София</p>
+                    <p className='elipse'>юни </p>
+                    </div>
+                    <p>Валидна до: 01 юни 2025</p>
+                    <div className='ads-btns'>
+                    <button className='ads-btn red'>Редактирай</button>
+                    <button className='ads-btn green'>Изтрий</button>
+                    </div>
                 </div>
             </section>
+            
         </div>
+        </>
     );
 }
