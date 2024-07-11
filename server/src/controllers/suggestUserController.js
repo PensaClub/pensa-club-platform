a5589b509ae387c0bd6e5404cf367bf900f681bd
SimonaG <ilievas95@gmail.com>
2024-07-11 21:41:51 +0300

    BE: Setup additional endpoints
    resolved/unresolved
    delete entry
    check if user has admin role

--- server/src/controllers/suggestUserController.js
+++ server/src/controllers/suggestUserController.js
@@ -1,6 +1,8 @@
 const suggestUserController = require('express').Router();
 
 const { user_suggest } = require('../sequelize/models/index');
+const isAuth = require('../middlewares/isAuth.js');
+const rbac = require('../middlewares/rbac');
 const { where } = require('sequelize');
 const fieldSwap = require('../utils/fieldSwap.js');
 const eventEmitter = require('../utils/eventEmitter.js');
@@ -30,13 +32,50 @@ suggestUserController.post('/', async (req, res, next) => {
   }
 });
 
-suggestUserController.get('/', async (req, res, next) => {
+suggestUserController.get('/resolved',rbac.checkPermission('approve_record'), isAuth, async (req, res, next) => {
   try {
     const userData = await user_suggest.findAll({
-      attributes: ['name', 'phone_number', 'message', 'resolved'],
+      where: { resolved: true },
     });
 
+    if (userData.length === 0) {
+      return res.status(404).json({ message: 'No resolved suggestions found' });
+    }
+
     res.status(200).json({ message: 'Suggested Users data retrieved successfully.', userData });
+  } catch (err) {
+    next(err);
+  }
+});
+suggestUserController.get('/unresolved',rbac.checkPermission('approve_record'), isAuth, async (req, res, next) => {
+  try {
+    const userData = await user_suggest.findAll({
+      where: { resolved: false },
+    });
+    
+    if (userData.length === 0) {
+      return res.status(404).json({ message: 'No unresolved suggestions found' });
+    }
+
+    res.status(200).json({ message: 'Suggested Users data retrieved successfully.', userData });
+  } catch (err) {
+    next(err);
+  }
+});
+
+suggestUserController.post('/delete', rbac.checkPermission('delete_record'), isAuth, async (req, res, next) => {
+  try {
+    const { id } = req.body;
+    const entry = await user_suggest.findOne({ where: { id } });
+    if (!entry) {
+      res.status(400).json({ message: "ID doesn't match an existing entry." });    }
+    
+      await entry.destroy();
+
+      eventEmitter.emit('entryDeleted', entry);
+
+      res.status(200).json({ message: 'Suggestion has been deleted successfully.' });
+    
   } catch (err) {
     next(err);
   }
