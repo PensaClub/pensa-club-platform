843353569411b218197fa18840e269e1707f07ec
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-07-16 19:57:43 +0300

    Refactor ads model and controller for better user experience
    This commit centralizes the restructuring of the `AdsCard` and `user_ads` models. By adopting a more straightforward approach of using a boolean `approved` field instead of a string `status` field, handling of ads approval becomes less prone to errors. The implementation of authentication check upon ad click further enhances UX by opening a notification for unauthenticated users instead of navigating to the ad directly.

--- client/src/components/Community/AdsCard/AdsCard.jsx
+++ client/src/components/Community/AdsCard/AdsCard.jsx
@@ -4,6 +4,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import { AdsCardSkeleton } from '../AdsCardSkeleton/AdsCardSkeleton';
 import { useTranslation } from 'react-i18next';
+import { useAuthContext } from '../../contexts/UserContext';
+import { useNavigate } from 'react-router-dom';
+import { AdModalNotify } from './AdModalNotify';
 
 const ImageModal = ({ src, alt, onClose }) => (
     <div className="image-modal-overlay" onClick={onClose}>
@@ -15,8 +18,12 @@ const ImageModal = ({ src, alt, onClose }) => (
 );
 
 export const AdsCard = ({ ads, isLoading }) => {
+    const{isAuthentication}=useAuthContext();
+    console.error(isAuthentication)
     const [modalImage, setModalImage] = useState(null);
+    const [open,setOpen] =useState(false)
     const { t } = useTranslation();
+    const navigate=useNavigate()
     if (isLoading) {
         return (
             <section className="ads-main">
@@ -30,21 +37,30 @@ export const AdsCard = ({ ads, isLoading }) => {
     const handleImageClick = (image) => {
         setModalImage(image);
     };
-
+    const handleAdClick= (ad) => {
+        if(isAuthentication) {
+            navigate(`/ads/${ad.adId}`)
+        }else{
+            setOpen(true);
+        }
+    }
     const closeModal = () => {
         setModalImage(null);
     };
+    const closeNotify=()=>{
+        setOpen(false)
+    }
 
     return (
         <>
             <section className="ads-main">
                 {ads.result.map(ad => (
-                    <div key={ad.adId} className="ads-card">
+                    <div key={ad.adId} className="ads-card" >
                         <div className="img-ads" onClick={() => handleImageClick(ad.images[0].imageURL)}>
                             <img src={ad.images[0].imageURL} alt={ad.summary} />
                             <p>{t(`search-criteria.${ad.category}`)}</p>
                         </div>
-                        <div className="ads-info">
+                        <div className="ads-info" onClick={()=>handleAdClick(ad)}>
                             <h3 className="title-card">{ad.summary}</h3>
                             <div className="subinfo-ads">
                                 {ad.tags.length > 0 && ad.tags.map(tag => (<p key={(tag)+1}>{"#"}{tag}</p>))}
@@ -60,6 +76,11 @@ export const AdsCard = ({ ads, isLoading }) => {
                     </div>
                 ))}
             </section>
+            {open && !isAuthentication && (
+                <AdModalNotify
+                    onClose={closeNotify}
+                />
+            )}
             {modalImage && (
                 <ImageModal
                     src={modalImage}
