f3932291971af2192a00189efea38749cc07c720
nymuelis <daniel.tsv.georgiev@gmail.com>
2024-07-25 18:23:45 +0300

    Add translation to new home page

--- client/src/components/Home/LastPosts/LastPosts.jsx
+++ client/src/components/Home/LastPosts/LastPosts.jsx
@@ -8,7 +8,7 @@ export const LastPosts = () => {
   const { getLatestAds } = useCommunityContext();
   const [latestAds, setLatestAds] = useState();
   const { t } = useTranslation();
-
+  const [categories, setCategories] = useState([]);
   useEffect(() => {
     window.scrollTo({ top: 0 });
 
@@ -17,11 +17,22 @@ export const LastPosts = () => {
       setLatestAds(ads);
     };
 
+    const fetchCategories = async () => {
+      const response = await fetch("/search-criteria.json");
+      const data = await response.json();
+      setCategories(data.searchCriteria);
+    };
+
     fetchLatestAds();
+    fetchCategories();
   }, []);
+  const translateCategory = (value) => {
+    const category = categories.find((cat) => cat.value === value);
+    return category ? t(category.name) : value;
+  };
   return (
     <section className="last-posts">
-      <h2>Последните наши публикации</h2>
+      <h2> {t("home.our_latest_ads")}</h2>
       <section className="home-posts">
         {latestAds &&
           latestAds.map((ad, index) => (
@@ -32,7 +43,9 @@ export const LastPosts = () => {
               <img src={ad.images[0].firebaseImagePath} alt={`card-${index}`} />
               <div className="card-info">
                 <p className="post-desc">{ad.description}</p>
-                <h4 className="post-category">{t(ad.category)}</h4>
+                <h4 className="post-category">
+                  {translateCategory(ad.category)}
+                </h4>
                 <div className="color-line-second"></div>
               </div>
               <div className="color-line"></div>
@@ -44,7 +57,7 @@ export const LastPosts = () => {
             className="btn-general btn-orange"
             id="more-btn"
           >
-            Вижте още публикации
+            {t("home.see_more_ads")}
           </Link>
         </div>
       </section>
