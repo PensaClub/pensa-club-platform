2c043128cc58c7639d17862796a6b194c6abbc26
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-07-25 20:39:44 +0300

    Update AllUsersStatistics.jsx

--- client/src/components/AdminDashboard/AllUsersStatistics/AllUsersStatistics.jsx
+++ client/src/components/AdminDashboard/AllUsersStatistics/AllUsersStatistics.jsx
@@ -290,12 +290,18 @@ export const AllUsersStatistics = () => {
         </div>
         <ResponsiveContainer width="100%" height={300}>
           <AreaChart data={registrationData}>
+            <defs>
+              <linearGradient id="colorCount" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#3498db" stopOpacity={0.8} />
+        <stop offset="95%" stopColor="#3498db" stopOpacity={0.2} /> 
+              </linearGradient>
+            </defs>
             <CartesianGrid strokeDasharray="3 3" />
             <XAxis dataKey="date" />
             <YAxis />
             <Tooltip content={renderCustomTooltip} />
             <Legend onClick={(e) => handleLegendClickLine(e.dataKey)} />
-            <Area type="monotone" dataKey="count" stroke="#82ca9d" strokeOpacity={hiddenLine.includes('count') ? 0.2 : 1} />
+            <Area type="monotone" dataKey="count" stroke="#82ca9d" fill="url(#colorCount)" fillOpacity={hiddenLine.includes('count') ? 0.2 : 1} />
           </AreaChart>
         </ResponsiveContainer>
       </div>
