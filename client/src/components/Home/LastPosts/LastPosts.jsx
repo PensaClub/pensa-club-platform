d253beeae2a4b95168edda6c5d2da2d17b710cd4
nymuelis <daniel.tsv.georgiev@gmail.com>
2024-07-26 12:38:32 +0300

    Final fixes to home page

--- client/src/components/Home/LastPosts/LastPosts.jsx
+++ client/src/components/Home/LastPosts/LastPosts.jsx
@@ -13,8 +13,8 @@ export const LastPosts = () => {
     window.scrollTo({ top: 0 });
 
     const fetchLatestAds = async () => {
-      const ads = await getLatestAds();
-      setLatestAds(ads);
+      const { result } = await getLatestAds();
+      setLatestAds(result);
     };
 
     const fetchCategories = async () => {
