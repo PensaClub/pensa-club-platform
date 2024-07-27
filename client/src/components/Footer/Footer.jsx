543581a63a53678c413b14113bdf59e5d97b59f5
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-07-27 08:14:09 +0300

    Update footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,19 +1,21 @@
 import './footer.css'
 import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
 
-export const Footer = ({additionalClasses}) => {
-    const {t} = useTranslation();
+export const Footer = ({ additionalClasses }) => {
+    const { t } = useTranslation();
 
     return (
         <footer className={additionalClasses}>
-            
+
             <section className="footer">
                 <img src="/images/homePage/logo-2.png" alt="logo" />
                 <div className="footer-links">
-                    <h3>{t('footer.site-map')}</h3>
-                    <p>{t('footer.communities')}</p>
-                    <p>{t('footer.map')}</p>
-                    <p>{t('footer.craigslist')}</p>
+                <h3>{t('footer.site-map')}</h3>
+                <p><Link to="/map">{t('footer.map')}</Link></p>
+                <p><Link to="/craigslist">{t('footer.craigslist')}</Link></p>
+                    {/* <p>{t('footer.communities')}</p> */}
+
                 </div>
                 <div className="footer-links">
                     <h3>{t('footer.partners')}</h3>
@@ -25,12 +27,12 @@ export const Footer = ({additionalClasses}) => {
                         <h3>Pensa club &copy;</h3>
                     </div>
                     <div className="info-desc">
-                    <p>{t('footer.country-city')}</p>
-                
-                    <p>{t('footer.street')}</p> 
-                 
-                    <p>{t('footer.phone')}</p> 
-                    <p>{t('footer.email')}</p> 
+                        <p>{t('footer.country-city')}</p>
+
+                        <p>{t('footer.street')}</p>
+
+                        <p>{t('footer.phone')}</p>
+                        <p>{t('footer.email')}</p>
                     </div>
                 </div>
             </section>
