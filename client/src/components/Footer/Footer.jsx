e4006fe5985e1015c1e5dfe89b17c37bd16ebb46
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-07-27 08:31:50 +0300

    add footer translation

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,10 +1,11 @@
 import './footer.css'
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
+import { useAuthContext } from '../contexts/UserContext';
 
 export const Footer = ({ additionalClasses }) => {
     const { t } = useTranslation();
-
+    const {isAuthentication} =useAuthContext();
     return (
         <footer className={additionalClasses}>
 
@@ -14,7 +15,7 @@ export const Footer = ({ additionalClasses }) => {
                 <h3>{t('footer.site-map')}</h3>
                 <p><Link to="/map">{t('footer.map')}</Link></p>
                 <p><Link to="/craigslist">{t('footer.craigslist')}</Link></p>
-                    {/* <p>{t('footer.communities')}</p> */}
+                {isAuthentication &&(<p><Link to="/ad/create">{t('footer.add-ad')}</Link></p>)}
 
                 </div>
                 <div className="footer-links">
