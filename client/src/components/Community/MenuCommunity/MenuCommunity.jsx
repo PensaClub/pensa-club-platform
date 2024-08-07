75b5e65c558a3755fb7a578e1c654c3b31d24a3c
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-08-07 09:18:51 +0300

    Implemented filter reset functionality on /craigslist route
    : The code changes serve to incorporate a filter reset function on the /craigslist path. This feature ensures that user filters are cleaned up when a user navigates away from Craigslist's page, improving the user experience by starting from a fresh state.
    
    This is achieved by adding a "reset" parameter to the page's path, which the react-router's useLocation hook registered for a change. A conditional logic was put in place to detect the reset parameter and subsequently reset the filters, as well as modify the website's route to /craigslist upon detection. To facilitate this functionality, adjustments were made to the URLSearchParams hook, as well as the handleSearch function. In addition, the navigation bar icons are now responsive to a click event that triggers a page reload.
    
    It's worth mentioning that the changes are backwards compatible and the application continues to function as expected if "/craigslist?reset=true" is not used.
    
    No particular issues are associated with this commit.

--- client/src/components/Community/MenuCommunity/MenuCommunity.jsx
+++ client/src/components/Community/MenuCommunity/MenuCommunity.jsx
@@ -1,38 +1,42 @@
 import "./menuCommunity.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMapLocation, faHouseUser, faUser, faBars } from '@fortawesome/free-solid-svg-icons';
 import { useLocation, useNavigate } from "react-router-dom";
 import { useAuthContext } from "../../contexts/UserContext";
 
 export const MenuCommunity = () => {
-
     const { isAuthentication } = useAuthContext();
     const location = useLocation();
     const navigate = useNavigate();
+    
     const handleNavigation = (path) => {
         navigate(path);
     };
+    
     const handleUserClick = () => {
         if (!isAuthentication) {
-
-            navigate('/profile')
+            navigate('/profile');
+        } else {
+            navigate('/sign-up');
         }
-        navigate('/sign-up')
-    }
+    };
+
+    const handleReloadPage = () => {
+        navigate('/craigslist?reset=true');
+    };
 
     const getLocation = (path) => {
         return location.pathname === path ? 'commun-menu-icons active' : 'commun-menu-icons';
-    }
+    };
 
     return (
         <>
             <nav className="menu-community">
                 <FontAwesomeIcon icon={faHouseUser} className={getLocation('/')} onClick={() => handleNavigation('/')} />
                 <FontAwesomeIcon icon={faMapLocation} className={getLocation('/map')} onClick={() => handleNavigation('/map')} />
-                <FontAwesomeIcon icon={faBars} className="commun-menu-icons"onClick={() => handleNavigation('/craigslist')} />
+                <FontAwesomeIcon icon={faBars} className={getLocation('/craigslist')} onClick={handleReloadPage} />
                 <FontAwesomeIcon icon={faUser} className={getLocation('/profile')} onClick={handleUserClick} />
             </nav>
         </>
-    )
-}
-
+    );
+};
