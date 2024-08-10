87d0af0e4da5474428e7b603396570a9ffb7c489
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-08-10 19:48:36 +0300

    Enhanced dropdown click handling in Profile CustomSelect
    The commit improves interactivity and navigation for CustomSelect dropdown options in the Profile component.
    
    A more reliable event listener, using useEffect, is now set up to close the dropdown when a click occurs outside it. This better manages the dropdown open and close states, improving experience for users and preventing unexpected interactions.
    
    The configuration of checkbox interactions is optimized to stop propagation of click events, preventing unchecked boxes from accidentally toggling the dropdown closure.
    
    These changes enhance user experience by increasing UI reliability and preventing potential issues in user interactions.
    
    Issue References: N/A

--- client/src/components/Profile/CustomSelect.jsx
+++ client/src/components/Profile/CustomSelect.jsx
@@ -1,13 +1,26 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons';
 import './CustomSelect.css';
 import { useTranslation } from 'react-i18next';
 
-const CustomSelect = ({ options, selectedOptions, onSelect}) => {
+const CustomSelect = ({ options, selectedOptions, onSelect }) => {
     const [isOpen, setIsOpen] = useState(false);
     const dropdownRef = useRef(null);
     const { t } = useTranslation();
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+                setIsOpen(false);
+            }
+        };
+
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
 
     const handleToggleDropdown = (e) => {
         e.preventDefault();
@@ -16,63 +29,64 @@ const CustomSelect = ({ options, selectedOptions, onSelect}) => {
 
     const handleCheckboxChange = (e) => {
         e.stopPropagation();
-
         const value = e.target.value;
-
         const newSelectedOptions = selectedOptions.includes(value)
             ? selectedOptions.filter(option => option !== value)
             : [...selectedOptions, value];
-
         onSelect(newSelectedOptions);
     };
 
-    const handleClickOutside = (event) => {
-        if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-            setIsOpen(false);
-        }
-    };
-    
     const handleSelectAll = (e) => {
-        e.preventDefault()
+        e.preventDefault();
         const allOptions = options.map(option => option.value);
         onSelect(allOptions);
     };
 
     const handleClearSelection = (e) => {
-        e.preventDefault()
+        e.preventDefault();
         onSelect([]);
     };
 
-    document.addEventListener('click', handleClickOutside);
+    const preventDefaultBehavior = (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+    };
 
     return (
-        <div className="custom-select-with-checkbox" ref={dropdownRef}>
+        <div className="custom-select-with-checkbox" ref={dropdownRef} onClick={preventDefaultBehavior}>
             <label className="custom-select-label"></label>
             <div className="custom-select-header" onClick={handleToggleDropdown}>
-                <span>{selectedOptions.length > 0 ? `${selectedOptions.length}  ${t('profile.selected')}` : t('profile.select')}</span>
-
+                <span>{selectedOptions.length > 0 ? `${selectedOptions.length} ${t('profile.selected')}` : t('profile.select')}</span>
                 <span className="custom-select-chevron">
                     <FontAwesomeIcon icon={isOpen ? faChevronUp : faChevronDown} />
                 </span>
             </div>
             {isOpen && (
-                <div className="custom-select-dropdown" onClick={(e) => e.stopPropagation()}>
-
+                <div className="custom-select-dropdown">
                     <div className="select-all-clear">
-                        <button onClick={handleSelectAll} className="select-all-btn">{t('profile.select_all')}</button>
-                        <button onClick={handleClearSelection} className="clear-btn">{t('profile.clear')}</button>
+                        <button
+                            onClick={handleSelectAll}
+                            className="select-all-btn"
+                        >
+                            {t('profile.select_all')}
+                        </button>
+                        <button
+                            onClick={handleClearSelection}
+                            className="clear-btn"
+                        >
+                            {t('profile.clear')}
+                        </button>
                     </div>
 
                     {options.map(option => (
-
                         <div key={option.value} className={`checkbox-item ${selectedOptions.includes(option.value) ? 'checked' : ''}`}>
-
                             <label>
                                 <input
                                     type="checkbox"
                                     value={option.value}
                                     checked={selectedOptions.includes(option.value)}
                                     onChange={handleCheckboxChange}
+                                    onClick={(e) => e.stopPropagation()} 
                                 />
                                 {t(option.name)}
                             </label>
@@ -80,7 +94,6 @@ const CustomSelect = ({ options, selectedOptions, onSelect}) => {
                     ))}
                 </div>
             )}
-             
         </div>
     );
 };
