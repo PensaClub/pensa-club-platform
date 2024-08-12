c95adb5c5a7f4427c7ef0507b3276bc652a5c025
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-08-12 21:19:10 +0300

    Update footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -20,7 +20,9 @@ export const Footer = ({ additionalClasses }) => {
                 </div>
                 <div className="footer-links">
                     <h3>{t('footer.partners')}</h3>
-                    <p>{t('footer.softuni')}</p>
+                    <p><Link to={"https://buditel.softuni.bg/"} target="_blank">{t('footer.softuni_buditel')}</Link></p>
+                    <p><Link to={"https://costeffective.software/"} target="_blank">Cost-Effective Solutions</Link></p>
+                    <p><Link to={"https://softuni.bg/"} target="_blank">{t('footer.softuni')}</Link></p>
                 </div>
                 <div className="footer-info">
                     <div className="second-info">
