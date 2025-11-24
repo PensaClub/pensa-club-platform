26c9206012955efa00353c8da96cc8fcaed73a4b
Borislav Iliev <borislaviliev47@gmail.com>
2025-11-24 17:58:10 +0200

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -4,9 +4,10 @@ const express = require("express");
 const expressConfig = require("./config/expressConfig");
 
 const router = require("./router");
-
+const botDetector = require("./middlewares/botDetector");
 const app = express();
 
 expressConfig(app);
-
+app.use(botDetector);
 app.use(router);
+module.exports = app;
